a038b8b4e53b9f7750a25fed133f38c0dc754939
Your Name <you@example.com>
2021-03-09 18:29:43 +0530

    fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ release = '0.1a0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser', "sphinx.ext.autodoc"]
+extensions = ['myst_parser', "sphinx.ext.autodoc", "sphinx_autodoc_typehints"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -39,7 +39,6 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
-
 
 # -- Options for HTML output -------------------------------------------------
 
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -4,8 +4,9 @@ IBLPy Documentation
 .. toctree::
    :maxdepth: 2
    :hidden:
-  
+
    BotClient
+   IBLPyBaseClasses
 
 IBLPy is a API wrapper/library around https://infinitybots.xyz, which is a great novel bot list for the next generation of bots owned by ``☣ Tσxιƈ Dҽʋ ☣#7308``.
 
