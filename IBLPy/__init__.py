e75a2d06b42dd20ca6db5044a2fff12eee5716bb
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-17 01:25:43 +0530

    Update __init__.py

--- IBLPy/__init__.py
+++ IBLPy/__init__.py
@@ -1,20 +1,10 @@
 api_modes = []
 try:
     import aiohttp
-    from aiohttp_requests import requests as requests_async
     api_modes.append("async")
-except:
-    requests_async = None
-    class aiohttp():
-        class ClientSession():
-            pass
-
-try:
-    import requests
-    api_modes.append("sync")
-except:
-    requests = None
-
+except Exception:
+    raise ImportError("You must have aiohttp installed to import this library!")
+    
 try:
     import uvicorn
     import fastapi
@@ -23,22 +13,15 @@ except:
     uvicorn = None
     fastapi = None
 
-if api_modes == []:
-    raise ibl_base.NoSuitableModesFound()
-
 try:
     import discord
-    autoposter = True
-except:
-    class discord():
-        class Client():
-            pass
-    autoposter = False
+    api_modes.append("autoposter")
+except Exception:
+    discord = None
 
 from IBLPy.base_fn import *
 from IBLPy.main import *
 from IBLPy.config import *
-from IBLPy.api_brain import *
 from IBLPy.ws import *
 
 __version__ = version
