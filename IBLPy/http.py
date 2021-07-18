d58193b465d136af2d18092fbf8b76f90a47838b
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-18 22:59:14 +0530

    Update http.py

--- IBLPy/http.py
+++ IBLPy/http.py
@@ -1,5 +1,5 @@
 import IBLPy.config as cfg
-from IBLPy.base_fn import IBLAPIResponse, IBLAPIRatelimit, IBLBot, IBLUser
+from IBLPy.classes import IBLAPIResponse, IBLAPIRatelimit, IBLBot, IBLUser
 import aiohttp
 from typing import Optional
 from loguru import logger
@@ -12,12 +12,31 @@ except:
 class BaseHTTP():
     user_agent = f"IBLPy/{cfg.version}"       
     
-    async def _request(self, method, endpoint, json, headers, auth = None):
+    def __init__(self, id):
+        self.id = id
+        self.logged_in = False 
+
+    def login(self, api_token):
+        """Logs in to the IBL API"""
+        self.api_token = api_token
+        self.logged_in = True
+    
+    async def request(
+        self, 
+        method: str, 
+        endpoint: str,
+        json: Optional[dict] = None, 
+        headers: Optional[dict] = None, 
+        auth: bool = False
+    ):
         """Makes a API request"""
         headers = {} if not headers else headers
         
         if auth:
-            headers["authorization"] = auth
+            if not self.logged_in:
+                raise ValueError("Not logged in to IBL yet")
+                
+            headers["authorization"] = self.api_token
         
         headers["User-Agent"] = self.user_agent
         
@@ -42,79 +61,47 @@ class BaseHTTP():
                 return IBLAPIResponse(raw_res = res, success = success, json = json, message = message, status = res.status)
      
     
-class BotHTTP(BaseHTTP):
-       
-    def __init__(self, bot_id):
-        self.bot_id = bot_id
-        self.logged_in = False 
-
-    def login(self, api_token):
-        """Logs in to the IBL API"""
-        self.api_token = api_token
-        self.logged_in = True
-        
-        
+class BotHTTP(BaseHTTP):    
     async def set_stats(
         self,
         guild_count: int, 
         shard_count: Optional[int] = None
-    ):
-        if not self.logged_in:
-            raise ValueError("Not logged in to IBL yet")
-        
-        # Fix a IBL API bug
-        shard_count = shard_count if shard_count else "0"
-        
-        json = {"servers": guild_count, "shards": shard_count}
-        return await self._request(
+    ):        
+        return await self.request(
             method="POST",
-            endpoint=f"/bot/{self.bot_id}",
-            json=json,
-            headers=None, 
-            auth=self.api_token
+            endpoint=f"/bot/{self.id}",
+            json={
+                "servers": guild_count,
+                "shards": shard_count if shard_count else "0" # IBL API workaroud
+            },
+            auth=True
         )
-
-    
+   
     async def get_bot(self):
         api_res = await self._request(
             method="GET",
-            endpoint=f"/bot/{self.bot_id}",
-            json=None,
-            headers=None,
+            endpoint=f"/bot/{self.id}",
         )
         
         json = api_res.json
         
         if json.get("error"):
             return api_res
-    
-        logger.debug(str(json))
         
-        return IBLBot(self.bot_id, json)
-
+        return IBLBot(self.id, json)
 
     
-async def get_user(user_id: int, debug: bool = False):
-    headers = {"User-Agent": user_agent, "Content-Type": "application/json"}
-    res = await requests_async.get(f"{cfg.api}/user/{user_id}", headers = headers)
-    # Workaround requests.json() sometimes giving string instead of dict due to IBL putting \\'s in API Responses and breaking Python JSON serialization
-    try:
-        json = json_lib.loads((await res.json()))
-    except:
-        try:
-            json = await res.json()
-        except:
-            json = {"message": "IBLPy: Could not deserialize data. Potential server error or malformed request", "error": True}
-
-    if res.status == 429 and error_on_ratelimit:
-        raise IBLAPIRatelimit()
-    if res.status == 429:
-        return IBLAPIResponse(raw_res = res, success = False, data = json, message = json.get("message"), status = res.status)
-    if json.get("error"):
-        return None
-    if debug:
-        print(f"DEBUG: {json}")
-    del json["error"] # Delete the error
-    json["id"] = user_id
-    return IBLUser(**json)
+class UserHTTP(BaseHTTP):         
+    async def get_user(user_id: int, debug: bool = False):
+        res = await self.request(
+            method="GET",
+            endpoint=f"{cfg.api}/user/{self.id}", 
+        )
+    
+        json = api_res.json
+        
+        if json.get("error"):
+            return api_res
+    
+        return IBLUser(self.id, json)
 
