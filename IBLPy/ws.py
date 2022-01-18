4af5f688df85fffbb8f774be09020726654464fd
Rootspring <66250972+cheesycod@users.noreply.github.com>
2022-01-18 22:58:29 +0530

    ws security

--- IBLPy/ws.py
+++ IBLPy/ws.py
@@ -1,76 +1,80 @@
-try:
-    from fastapi import APIRouter, Header, Request
-    from starlette.exceptions import HTTPException as StarletteHTTPException
-    from pydantic import BaseModel
-    from typing import Optional, Union
+from fastapi import APIRouter, Header, Request
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from pydantic import BaseModel
+from typing import Optional, Union
 
-    router = APIRouter(include_in_schema = False)
+router = APIRouter()
 
-    def abort(code: int) -> StarletteHTTPException:
-        raise StarletteHTTPException(status_code=code)
+def abort(code: int) -> StarletteHTTPException:
+    raise StarletteHTTPException(status_code=code)
 
-    class VoteInternal(BaseModel):
-        """
-            Represents a internal IBL Vote. IBLPy will make this a Vote class
-        """
-        timeStamp: int
-        userID: str
-        userName: str
-        botID: str
-        type: str
-        count: Union[int, str] = 0
+def secure_strcmp(val1, val2):
+    """
+    From Django:
+
+    Return True if the two strings are equal, False otherwise. This is a secure function
+    """
+    return secrets.compare_digest(val1, val2)
 
 
-    class Vote():
-        """
-            Represents a vote on IBL
+class VoteInternal(BaseModel):
+    """
+        Represents a internal IBL Vote. IBLPy will make this a Vote class
+    """
+    timeStamp: int
+    userID: str
+    userName: str
+    botID: str
+    type: str
+    count: Union[int, str] = 0
 
-            :param bot_id: The Bot ID of the vote
 
-            :param user_id: The ID of the user who voted for your bot. In test mode, this will be 0
+class Vote():
+    """
+        Represents a vote on IBL
 
-            :param username: The username who voted for your bot
+        :param bot_id: The Bot ID of the vote
 
-            :param count: The amount of votes your bot now has
+        :param user_id: The ID of the user who voted for your bot. In test mode, this will be 0
 
-            :param test: Whether this is a test webhook or not
+        :param username: The username who voted for your bot
 
-            :param timestamp: The timestamp (epoch) when the vote happened
-        """
-        def __init__(self, bot_id: int, user_id: int, test: bool, timestamp: int, count: int, username: str):
-            self.bot_id = bot_id
-            self.user_id = user_id
-            self.test = test
-            self.timestamp = timestamp
+        :param count: The amount of votes your bot now has
 
-            if count.isdigit():
-                self.count = int(count)
-            else:
-                self.count = 0
+        :param test: Whether this is a test webhook or not
 
-    @router.post("/_dbg")
-    async def debug_webhook(request: Request):
-        print((await request.body()), secret)
+        :param timestamp: The timestamp (epoch) when the vote happened
+    """
+    def __init__(self, bot_id: int, user_id: int, test: bool, timestamp: int, count: int, username: str):
+        self.bot_id = bot_id
+        self.user_id = user_id
+        self.test = test
+        self.timestamp = timestamp
 
-    @router.post("/")
-    async def iblpy_webhook(vote_internal: VoteInternal, Authorization: str = Header("INVALID_SECRET")):
-        if secret is None or secret == Authorization:
-            pass
+        if count.isdigit():
+            self.count = int(count)
         else:
-            return abort(401)
-        timestamp = int(vote_internal.timeStamp)
-        
-        if vote_internal.type.lower() == "test":
-            bot_id = botcli.id
-            user_id = 0
-            test = True
-        else:
-            bot_id = int(vote_internal.botID)
-            user_id = int(vote_internal.userID)
-            test = False
-        vote = Vote(bot_id = bot_id, user_id = user_id, test = test, timestamp = timestamp, count = vote_internal.count, username = vote_internal.userName)
-        return await wh_func(vote, secret)
+            self.count = 0
 
-except:
-    pass
+@router.post("/_dbg")
+async def debug_webhook(request: Request):
+    print((await request.body()), secret)
 
+@router.post("/")
+async def iblpy_webhook(vote_internal: VoteInternal, Authorization: str = Header("INVALID_SECRET")):
+    if secret is None or secure_strcmp(secret, Authorization):
+        pass
+    else:
+        return abort(401)
+    timestamp = int(vote_internal.timeStamp)
+
+    if vote_internal.type.lower() == "test":
+        bot_id = botcli.id
+        user_id = 0
+        test = True
+    else:
+        bot_id = int(vote_internal.botID)
+        user_id = int(vote_internal.userID)
+        test = False
+    vote = Vote(bot_id = bot_id, user_id = user_id, test = test, timestamp = timestamp, count = vote_internal.count, username = vote_internal.userName)
+    return await wh_func(vote, secret)
